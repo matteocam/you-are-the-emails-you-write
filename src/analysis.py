effc359908ba13f1c89865b7810ca8b11145a3e6
matteocam <matteo.campanelli@gmail.com>
2014-12-18 03:24:23 -0500

    Added filtering methods

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,7 @@
 import nltk
 import pickle
 import random
+import re
 
 # contains a list of all emails
 #allMails = None
@@ -39,14 +40,64 @@ def main():
     
 
 class Mails:
+    endlineSep = '\r\n'
     def __init__(self, _allMails):
         self.allMails = _allMails
+        self.allMails_raw = allMails
+        
+        self.cleanupEmails()
+        
     
     def getRandomMail(self):
         return random.choice(self.allMails)
         
+    def cleanupEmails(self):
+        " Remove emails with None fields and filter bodies"
+        
+        # If some emails are drafts they may have None as rcpt
+        rcptNotNone = lambda m: m['to'] is not None
+        self.allMails = filter(rcptNotNone, self.allMails)
+        
+        self.allMails = map(Mails.removeQuotingFromBody, self.allMails)
+        self.allMails = map(Mails.removeURLsFromBody, self.allMails)
+        
     def filterPersonalEmails(self):
-        pass
+        return filter(Mails.isPersonal, self.allMails)
 
+    @staticmethod    
+    def isPersonal(m):
+        # NOTE: only english recipients for now
+        personalRecipients = ["annika", "kelly", "sofia", "ana",
+                              "konst", "anoop", "floris", "nigel" ]
+        recipientLowercase = m['to'].lower()
+        found = any([recipientLowercase.find(r) != -1  
+                        for r in personalRecipients])
+        
+        return found
 
-
+    @staticmethod
+    def removeQuotingFromBody(m):
+        body = m['body']
+        
+        lines = body.split(Mails.endlineSep)
+        notAQuoteLine = lambda l: l == '' or l[0] != '>'
+        # remove quote lines
+        onlyMyLines = filter(notAQuoteLine, lines)
+        # remove lines of type "On  Thu, Nov 20, 2014 at 12:15 PM, xxx wrote:"
+        notThatKindOfLine = lambda l: not re.search("^On.*20.*M.*", l)
+        notThatKindOfLine_bis = lambda l: not re.search(".*@.*> wrote:", l)
+        
+        onlyMyLines = filter(notThatKindOfLine, onlyMyLines)
+        onlyMyLines = filter(notThatKindOfLine_bis, onlyMyLines)
+        # remove lines of type "2013/3/7 guy <guy@xxx>"
+        notThatKindOfLine2 = lambda l: not re.search("^20..[/-].*[/-].*<.*@.*>", l)
+        onlyMyLines = filter(notThatKindOfLine2, onlyMyLines)
+        m['body'] = Mails.endlineSep.join(onlyMyLines)
+        return m
+                
+    @staticmethod
+    def removeURLsFromBody(m):
+        body = m['body']
+        m['body'] = re.sub(r'\<http.+?\>', '', body, flags=re.S)
+        return m
+        

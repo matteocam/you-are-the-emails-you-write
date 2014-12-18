dc6f20be9a943a10718c14b83a49abd9bd4b45fa
matteocam <matteo.campanelli@gmail.com>
2014-12-18 00:41:51 -0500

    Created class Mails. Started filtering.

--- src/analysis.py
+++ src/analysis.py
@@ -1,12 +1,17 @@
 import nltk
 import pickle
+import random
+
+# contains a list of all emails
+#allMails = None
 
 class DataLoading:
   @staticmethod
   def loadDataFromArchive():
     # Loads data from archives
     # XXX: currently only archive 3
-    f = open("mail_archive3", 'r')
+    archiveName = "../archives/all_mails_archive"
+    f = open(archiveName, 'r')
     mails = pickle.load(f)
     return mails
 
@@ -28,6 +33,20 @@ class FrequencyAnalysis:
   def __init__(self, mails):
     self.mails = mails
 
+def main():
+    global allMails
+    allMails = DataLoading.loadDataFromArchive()
+    
+
+class Mails:
+    def __init__(self, _allMails):
+        self.allMails = _allMails
+    
+    def getRandomMail(self):
+        return random.choice(self.allMails)
+        
+    def filterPersonalEmails(self):
+        pass
 
 
 
